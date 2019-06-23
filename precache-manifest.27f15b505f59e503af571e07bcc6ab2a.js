b66c6650526e7b51c7c56e9f002e3d04dce8a2a3
Adam Darmain <adam.darmanin@aurea.com>
2019-06-23 23:02:21 +0200

    adding static html

---
+++ precache-manifest.27f15b505f59e503af571e07bcc6ab2a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa9cc33d8b4f362c95e15e8bceba6081",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8b2fc9d4cfc263026df8",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "8b2fc9d4cfc263026df8",
+    "url": "/static/js/2.31e6a09b.chunk.js"
+  },
+  {
+    "revision": "b6804d0649088a568267",
+    "url": "/static/js/main.53fc57b6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
